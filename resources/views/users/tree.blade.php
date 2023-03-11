cc205fe4fa7393e1262f765a6414fce2df717747
Rachiket Arya <rachiket@tryjackett.com>
2023-03-11 19:47:01 +0530

    Add colour to male-female

--- resources/views/users/tree.blade.php
+++ resources/views/users/tree.blade.php
@@ -13,37 +13,37 @@ $ggccTotal = 0;
 ?>
 
 <div id="wrapper">
-    <span class="label">{{ link_to_route('users.tree', $user->name, [$user->id], ['title' => $user->name.' ('.$user->gender.')']) }}</span>
+    <span class="label {{ $user->gender }}">{{ link_to_route('users.tree', $user->name, [$user->id], ['title' => $user->name.' ('.$user->gender.')']) }}</span>
         @if ($childsCount = $user->childs->count())
         <?php $childsTotal += $childsCount ?>
         <div class="branch lv1">
             @foreach($user->childs as $child)
             <div class="entry {{ $childsCount == 1 ? 'sole' : '' }}">
-                <span class="label">{{ link_to_route('users.tree', $child->name, [$child->id], ['title' => $child->name.' ('.$child->gender.')']) }}</span>
+                <span class="label {{ $child->gender }}">{{ link_to_route('users.tree', $child->name, [$child->id], ['title' => $child->name.' ('.$child->gender.')']) }}</span>
                 @if ($grandsCount = $child->childs->count())
                 <?php $grandChildsTotal += $grandsCount ?>
                 <div class="branch lv2">
                     @foreach($child->childs as $grand)
                     <div class="entry {{ $grandsCount == 1 ? 'sole' : '' }}">
-                        <span class="label">{{ link_to_route('users.tree', $grand->name, [$grand->id], ['title' => $grand->name.' ('.$grand->gender.')']) }}</span>
+                        <span class="label {{ $grand->gender }}">{{ link_to_route('users.tree', $grand->name, [$grand->id], ['title' => $grand->name.' ('.$grand->gender.')']) }}</span>
                         @if ($ggCount = $grand->childs->count())
                         <?php $ggTotal += $ggCount ?>
                         <div class="branch lv3">
                             @foreach($grand->childs as $gg)
                             <div class="entry {{ $ggCount == 1 ? 'sole' : '' }}">
-                                <span class="label">{{ link_to_route('users.tree', $gg->name, [$gg->id], ['title' => $gg->name.' ('.$gg->gender.')']) }}</span>
+                                <span class="label {{ $gg->gender }}">{{ link_to_route('users.tree', $gg->name, [$gg->id], ['title' => $gg->name.' ('.$gg->gender.')']) }}</span>
                                 @if ($ggcCount = $gg->childs->count())
                                 <?php $ggcTotal += $ggcCount ?>
                                 <div class="branch lv4">
                                     @foreach($gg->childs as $ggc)
                                     <div class="entry {{ $ggcCount == 1 ? 'sole' : '' }}">
-                                        <span class="label">{{ link_to_route('users.tree', $ggc->name, [$ggc->id], ['title' => $ggc->name.' ('.$ggc->gender.')']) }}</span>
+                                        <span class="label {{ $ggc->gender }}">{{ link_to_route('users.tree', $ggc->name, [$ggc->id], ['title' => $ggc->name.' ('.$ggc->gender.')']) }}</span>
                                         @if ($ggccCount = $ggc->childs->count())
                                         <?php $ggccTotal += $ggccCount ?>
                                         <div class="branch lv5">
                                             @foreach($ggc->childs as $ggcc)
                                             <div class="entry {{ $ggccCount == 1 ? 'sole' : '' }}">
-                                                <span class="label">{{ link_to_route('users.tree', $ggcc->name, [$ggcc->id], ['title' => $ggcc->name.' ('.$ggcc->gender.')']) }}</span>
+                                                <span class="label {{ $ggcc->gender }}">{{ link_to_route('users.tree', $ggcc->name, [$ggcc->id], ['title' => $ggcc->name.' ('.$ggcc->gender.')']) }}</span>
                                             </div>
                                             @endforeach
                                         </div>
